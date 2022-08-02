b068e744c17d06e7ab4c9f10ec5a4f9e87072982
BaeHyunJun <hj3bae@gmail.com>
2022-08-02 16:46:57 +0900

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-// import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Image from "next/image";
 
 const Home: NextPage = () => {
   return (
@@ -61,7 +61,8 @@ const Home: NextPage = () => {
         >
           Powered by{' '}
           <span className={styles.logo}>
-            <img src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+            {/*<img src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />*/}
           </span>
         </a>
       </footer>
